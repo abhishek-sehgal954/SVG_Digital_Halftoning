22379458b0f141daead17bba48cc9c8a326b507b
Abhishek <sehgala954@gmail.com>
2017-06-08 12:02:21 +0530

    addedtriangular halftone pattern

--- Raster_to_SVG_halftone.py
+++ Raster_to_SVG_halftone.py
@@ -1,12 +1,13 @@
 import os
 import webbrowser
+import sys
 import svgwrite
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
 
-def intensity_2(image):
+def intensity_dot(image):
   arr=np.asarray(image)
   mini=999
   maxi=0
@@ -54,6 +55,91 @@ def intensity_2(image):
   browser.open_new('file://' + os.path.realpath("test.svg"))
 
 
+  #print gray_level[brr[0][0]]
+def intensity_triangle(image):
+  arr=np.asarray(image)
+  mini=999
+  maxi=0
+  for i in range(len(arr)):
+    for j in range(len(arr[0])):
+      maxi=max(arr[i][j],maxi)
+      mini=min(arr[i][j],mini)
+  level=float(float(maxi-mini)/float(10));
+  brr=[[0]*len(arr[0]) for i in range(len(arr))]
+  for i in range(10):
+    l1=mini+level*i
+    l2=l1+level
+    for j in range(len(arr)):
+      for k in range(len(arr[0])):
+        if(arr[j][k]>=l1 and arr[j][k]<=l2):
+          brr[j][k]=i
+  dwg = svgwrite.Drawing('test.svg', profile='full',size=('70cm', '70cm'))
+  startu=0
+  endu=0
+  for i in range(len(brr)):
+    for j in range(len(brr[i])):
+      temp_list=[]
+      if(brr[i][j]==0):
+       
+        temp_list.append((int((startu+startu+5)/2),endu))
+        temp_list.append((startu,endu+5))
+        temp_list.append((startu+5,endu+5))
+      elif(brr[i][j]==1):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.1))
+        temp_list.append((startu+0.1,endu+5-0.1))
+        temp_list.append((startu+5-0.1,endu+5-0.1))
+      elif(brr[i][j]==2):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.2))
+        temp_list.append((startu+0.2,endu+5-0.2))
+        temp_list.append((startu+5-0.2,endu+5-0.2))
+      elif(brr[i][j]==3):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.3))
+        temp_list.append((startu+0.3,endu+5-0.3))
+        temp_list.append((startu+5-0.3,endu+5-0.3))
+      elif(brr[i][j]==4):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.4))
+        temp_list.append((startu+0.4,endu+5-0.4))
+        temp_list.append((startu+5-0.4,endu+5-0.4))
+      elif(brr[i][j]==5):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.5))
+        temp_list.append((startu+0.5,endu+5-0.5))
+        temp_list.append((startu+5-0.5,endu+5-0.5))
+      elif(brr[i][j]==6):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.6))
+        temp_list.append((startu+0.6,endu+5-0.6))
+        temp_list.append((startu+5-0.6,endu+5-0.6))
+      elif(brr[i][j]==7):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.7))
+        temp_list.append((startu+0.7,endu+5-0.7))
+        temp_list.append((startu+5-0.7,endu+5-0.7))
+      elif(brr[i][j]==8):
+        #temp_list=[]
+        temp_list.append((int((startu+startu+5)/2),endu-0.8))
+        temp_list.append((startu+0.8,endu+5-0.8))
+        temp_list.append((startu+5-0.8,endu+5-0.8))
+      
+        
+
+      
+      #print temp_list
+      if(len(temp_list)!=0):
+        dwg.add(dwg.polygon(temp_list,fill='black'))
+      startu=startu+5
+      #print startu,endu
+    endu=endu+5
+    startu=0
+  dwg.save() 
+  browser=webbrowser.get('firefox')
+  browser.open_new('file://' + os.path.realpath("test.svg"))
+
+
 
       
 
@@ -61,7 +147,7 @@ def intensity_2(image):
 fname = 'test.jpg'
 image = Image.open(fname)
 image = Image.open(fname).convert('L')
-#intensity_1(image,3)
-#intensity_1(image,5)
-#intensity_1(image,7)
-intensity_2(image)
+if(sys.argv[1] == 'dot'):
+  intensity_dot(image)
+elif(sys.argv[1] == 'triangle'):
+  intensity_triangle(image)
