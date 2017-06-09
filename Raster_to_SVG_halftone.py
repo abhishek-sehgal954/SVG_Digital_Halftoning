cd4c617bd6b59b5976ffd82feb41cb5e5f0f0d0a
Abhishek <sehgala954@gmail.com>
2017-06-09 13:35:32 +0530

    added comments

--- Raster_to_SVG_halftone.py
+++ Raster_to_SVG_halftone.py
@@ -17,7 +17,7 @@ def intensity_dot(image):
       mini=min(arr[i][j],mini)
   level=float(float(maxi-mini)/float(10));
   brr=[[0]*len(arr[0]) for i in range(len(arr))]
-  for i in range(10):
+  for i in range(10):                                              #computing pixel intensity of each individual pixel on a scale of 0 to 9
     l1=mini+level*i
     l2=l1+level
     for j in range(len(arr)):
@@ -30,8 +30,8 @@ def intensity_dot(image):
   gray_level[0] = 2.5
   gray_level[1] = 2.2
   gray_level[2] = 2
-  gray_level[3] = 1.8
-  gray_level[4] = 1.6
+  gray_level[3] = 1.8                                              #mapping the intensity level to radius of circle
+  gray_level[4] = 1.6                                             
   gray_level[5]=  1.4
   gray_level[6] = 1.2
   gray_level[7] = 1
@@ -45,8 +45,8 @@ def intensity_dot(image):
   dwg = svgwrite.Drawing('test.svg', profile='full',size=('70cm', '70cm'))
   for i in range(len(brr)):
   	for j in range(len(brr[i])):
-  		dwg.add(dwg.circle((int((startu+startu+5)/2),int((endu+endu+5)/2)),gray_level[brr[i][j]]))
-  		startu=startu+5
+  		dwg.add(dwg.circle((int((startu+startu+5)/2),int((endu+endu+5)/2)),gray_level[brr[i][j]]))       #draw .svg file using circle of different 
+  		startu=startu+5                                                                                  #radius calculated using intensity levels.
   		#print startu,endu
   	endu=endu+5
   	startu=0
@@ -65,7 +65,7 @@ def intensity_triangle(image):
       maxi=max(arr[i][j],maxi)
       mini=min(arr[i][j],mini)
   level=float(float(maxi-mini)/float(10));
-  brr=[[0]*len(arr[0]) for i in range(len(arr))]
+  brr=[[0]*len(arr[0]) for i in range(len(arr))]                #computing pixel intensity of each individual pixel on a scale of 0 to 9
   for i in range(10):
     l1=mini+level*i
     l2=l1+level
@@ -87,8 +87,8 @@ def intensity_triangle(image):
       elif(brr[i][j]==1):
         #temp_list=[]
         temp_list.append((int((startu+startu+5)/2),endu+0.1))
-        temp_list.append((startu+0.1,endu+5-0.1))
-        temp_list.append((startu+5-0.1,endu+5-0.1))
+        temp_list.append((startu+0.1,endu+5-0.1))                                     #draw .svg file using triangle of different size
+        temp_list.append((startu+5-0.1,endu+5-0.1))                                   #calculated using intensity levels.
       elif(brr[i][j]==2):
         #temp_list=[]
         temp_list.append((int((startu+startu+5)/2),endu+0.2))
@@ -139,7 +139,7 @@ def intensity_triangle(image):
   browser=webbrowser.get('firefox')
   browser.open_new('file://' + os.path.realpath("test.svg"))
 
-def intensity_hexagon(image):
+def intensity_hexagon(image):                          
   arr=np.asarray(image)
   mini=999
   maxi=0
@@ -148,7 +148,7 @@ def intensity_hexagon(image):
       maxi=max(arr[i][j],maxi)
       mini=min(arr[i][j],mini)
   level=float(float(maxi-mini)/float(10));
-  brr=[[0]*len(arr[0]) for i in range(len(arr))]
+  brr=[[0]*len(arr[0]) for i in range(len(arr))]                         #computing pixel intensity of each individual pixel on a scale of 0 to 9
   for i in range(10):
     l1=mini+level*i
     l2=l1+level
@@ -175,8 +175,8 @@ def intensity_hexagon(image):
         temp_list.append((startu+2,endu+0.2))
         temp_list.append((startu+4,endu+0.2))
         temp_list.append((startu+0.2,endu+3))
-        temp_list.append((startu+6-0.2,endu+3))
-        temp_list.append((startu+2,endu+3-0.2))
+        temp_list.append((startu+6-0.2,endu+3))                #draw .svg file using hexagon of different size
+        temp_list.append((startu+2,endu+3-0.2))                #calculated using intensity levels.
         temp_list.append((startu+4,endu+3-0.2))
       elif(brr[i][j]==2):
         #temp_list=[]
