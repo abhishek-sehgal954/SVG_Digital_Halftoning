d026a9da454f82aa8a1bc5499d49370b81451e2c
Abhishek <sehgala954@gmail.com>
2017-06-22 17:43:52 +0530

    added main

--- SVG_to_SVG_halftone.py
+++ SVG_to_SVG_halftone.py
@@ -14,14 +14,17 @@ from PIL import Image
 import subprocess
 from Raster_to_SVG_halftone import clustered_dot, triangle, hexagon, dispersed_dot
 
-p=subprocess.call(['/usr/bin/inkscape', 'test_svg.svg', '--export-png', 'test_svg.jpg'])
-fname = 'test_svg.jpg'
-image = Image.open(fname).convert('L')
-if(sys.argv[1] == 'clustered_dot'):
-  clustered_dot(image)
-elif(sys.argv[1] == 'triangle'):
-  triangle(image)
-elif(sys.argv[1] == 'hexagon'):
-  hexagon(image)
-elif(sys.argv[1] == 'dispersed'):
-  dispersed_dot(image)	
+def main():
+	p=subprocess.call(['/usr/bin/inkscape', 'test_svg.svg', '--export-png', 'test_svg.jpg'])
+	fname = 'test_svg.jpg'
+	image = Image.open(fname).convert('L')
+	if(sys.argv[1] == 'clustered_dot'):
+  		clustered_dot(image)
+	elif(sys.argv[1] == 'triangle'):
+  		triangle(image)
+	elif(sys.argv[1] == 'hexagon'):
+  		hexagon(image)
+	elif(sys.argv[1] == 'dispersed'):
+  		dispersed_dot(image)	
+if __name__=="__main__":
+  main()
