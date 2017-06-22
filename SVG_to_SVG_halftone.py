845c478cef9952318f2f0627eebd77ce5e4e31e4
Abhishek <sehgala954@gmail.com>
2017-06-22 17:30:08 +0530

    added description

--- SVG_to_SVG_halftone.py
+++ SVG_to_SVG_halftone.py
@@ -1,23 +1,27 @@
+#This program takes a vector image and produces its vector halftone using patterning algorithm .
+# It supports 4 patterns 
+# 1. Clustered dot
+# 2. Triangle
+# 3. Hexagon
+# 4. Dispersed dot
+# output file produced is output.svg which can be opened with any SVG viewer (inkscape, imagemagick etc.)
+
 import os
-import webbrowser
 import sys
 import svgwrite
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 import subprocess
-from Raster_to_SVG_halftone import intensity_dot, intensity_triangle, intensity_hexagon, intensity_dispersed
+from Raster_to_SVG_halftone import clustered_dot, triangle, hexagon, dispersed_dot
 
-
-p=subprocess.call(['/usr/bin/inkscape', 'test2.svg', '--export-png', 'test2.png'])
-fname = 'test2.png'
-image = Image.open(fname)
+p=subprocess.call(['/usr/bin/inkscape', 'test_svg.svg', '--export-png', 'test_svg.jpg'])
+fname = 'test_svg.jpg'
 image = Image.open(fname).convert('L')
-if(sys.argv[1] == 'dot'):
-  intensity_dot(image)
+if(sys.argv[1] == 'clustered_dot'):
+  clustered_dot(image)
 elif(sys.argv[1] == 'triangle'):
-  intensity_triangle(image)
-elif(sys.argv[1]=='hexagon'):
-  intensity_hexagon(image)
-elif(sys.argv[1]=='dispersed'):
-  intensity_dispersed(image)	
+  triangle(image)
+elif(sys.argv[1] == 'hexagon'):
+  hexagon(image)
+elif(sys.argv[1] == 'dispersed'):
+  dispersed_dot(image)	
