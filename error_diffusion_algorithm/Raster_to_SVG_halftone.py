5e01bd15fa77712d3207f1822b20a7c03427e7c5
Abhishek <sehgala954@gmail.com>
2017-06-27 06:10:49 +0530

    added Raster_to_SVG_halftone.py

---
+++ error_diffusion_algorithm/Raster_to_SVG_halftone.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+import svgwrite
+from error_diffusion import error_dispersion
+
+def draw_svg(output):
+  startu = 0
+  endu = 0
+  dwg = svgwrite.Drawing('output.svg', profile = 'full')
+  for i in range(len(output)):
+  	for j in range(len(output[i])):
+  		if (output[i][j]==0):
+  			dwg.add(dwg.circle((int((startu+startu+1)/2),int((endu+endu+1)/2)),1))        
+  		startu = startu+2                                                                               
+  	endu = endu+2
+  	startu = 0
+  dwg.save() 
+
+def main():
+  fname = 'test.jpg'
+  image = Image.open(fname).convert('L')
+  output = error_dispersion(image)
+  draw_svg(output)
+
+if __name__=="__main__":
+  main()
+
