26dd062b77c22206e4b1024892b780f4ec97698b
Abhishek <sehgala954@gmail.com>
2017-07-05 08:22:16 +0530

    updated quality of subtractive color mixing

--- color_mixing.py
+++ color_mixing.py
@@ -2,12 +2,13 @@ import svgwrite
 import webbrowser
 import os
 
-dwg = svgwrite.Drawing('test.svg',profile='full',viewBox=("0 0 1500 1500"),id='f-multiply-opacity',preserveAspectRatio='xMinYMin meet')
+dwg = svgwrite.Drawing('test.svg',profile='full',size=('70cm','70cm'),id='f-multiply-opacity',preserveAspectRatio='xMinYMin meet')
+dwg.add(dwg.rect(insert=(0,0),size=("100%","100%"),fill='white'))
 blend = dwg.defs.add(dwg.filter(id="B4" ,filterUnits="objectBoundingBox", x="0%", y="0%", width="100%", height="100%"))
-blend.feBlend(in_='SourceGraphic', mode='screen')
+blend.feBlend(in_='BackgroundImage',in2="SourceGraphic", mode='multiply')
 dwg.add( dwg.rect(insert=(550, 550), size=(500, 500), fill='magenta'))
-dwg.add( dwg.rect(insert=(750, 600), size=(500, 500), opacity='0.5',fill='cyan',filter='url(#B4)' ))
-dwg.add( dwg.rect(insert=(600, 750), size=(500, 500), opacity='0.5',fill='yellow',filter='url(#B4)' ))
+dwg.add( dwg.rect(insert=(750, 600), size=(500, 500),fill='cyan',filter='url(#B4)' ))
+dwg.add( dwg.rect(insert=(600, 750), size=(500, 500),fill='yellow',filter='url(#B4)' ))
 dwg.save()
 browser=webbrowser.get('firefox')
 browser.open_new('file://' + os.path.realpath("test.svg")) 
